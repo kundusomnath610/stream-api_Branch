38ca06423c9e73b7390a0aab9c014c13c04cd92b
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-10 23:31:23 +0530

    Stream API 10 July

--- src/main/java/org/example/StreamObject.java
+++ src/main/java/org/example/StreamObject.java
@@ -1,5 +1,22 @@
 package org.example;
 
+
+/*
+ * 
+ * Creating the Stream Interface Object.
+ * With the help of Stream API...
+ * 
+ * We are performing the collection Object and Perform the Stream API in
+ * Array Also..
+ * 
+ * Their are lot of method to create the Stream API like 
+ * 1. ofNullable
+ * 2. of
+ * 3. empty
+ * 4. build
+ * All four are same to create to stream API
+ */
+
 public class StreamObject {
     public static void main(String[] args) {
         
